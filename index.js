14eef7c8878c912f02204c8cd485d5574486f4c1
pwela <139175113+pwela@users.noreply.github.com>
2023-08-10 23:51:33 -0600

    Exercise 2.5

--- index.js
+++ index.js
@@ -68,6 +68,42 @@ app.get('/',(req,res) => {
     res.send('Hola! Bienvenido! Welcome! Salute! Bienvenue!');
 });
 
+// Return data (description, genre, director, image URL, whether it’s featured or not)
+// about a single movie by title to the user
+app.get('/movies/:title/description', (req, res) => {
+    res.send('Get Request to return data (description, genre, director, image URL, whether it’s featured or not about a single movie by title to the user');
+});
+ 
+// Return data about a director (bio, birth year, death year) by name	
+app.get('/movies/:name/director', (req, res) =>{
+    res.send('GET request to return data about a director (bio, birth year, death year) by name');
+});
+
+// Allow new users to register
+app.post('/users', (req, res) =>{
+    res.send('POST requst to allow new users to register');
+});
+
+// Allow users to update their user info (username)	
+app.put('/users/:userId/:username', (req, res) =>{
+    res.send('Put requet to allow users to update their user info (username)');
+});
+
+// Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/movies/:title/:userId/favorites', (req, res) =>{
+    res.send('add request to allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)');
+});
+
+// Allow users to remove a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.delete('/movies/:title/:userId/favorites', (req, res) =>{
+    res.send('delete request to allow users to remove a movie to their list of favorites (showing only a text that a movie has been added—more on this later)');
+});
+
+// Allow new users to register
+app.delete('/users/:userId', (req, res) =>{
+    res.send('delete request to allow existing users to deregister');
+});
+
 
 // error loging
 app.use((err, req, res, next) => {
