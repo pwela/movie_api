fb59c059ef4cd7cb1c58bee2b3e9aaa84dbc1cb8
pwela <139175113+pwela@users.noreply.github.com>
2023-08-20 23:27:18 -0600

    Bug corrections
    Minors bug corrections have been done in models.js file.

--- models.js
+++ models.js
@@ -31,11 +31,11 @@ let userSchema = mongoose.Schema ({
 userSchema.statics.hashPassword = (password) =>{
     return bcrypt.hashSync(password, 10);
 
-}
+};
 
 userSchema.methods.validatePassword = function(password){
     return bcrypt.compareSync(password, this.password);
-}
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
