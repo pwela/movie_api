673eac231052576114b586bc0cb6c1f07c21b5ef
pwela <139175113+pwela@users.noreply.github.com>
2024-02-25 13:01:16 -0700

    add aws subdomain Regex in CORS

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ let alloweddOrigins = [
   // "http://load-balancer-task-2-2-1273969783.ca-central-1.elb.amazonaws.com/",
   // "http://myflixclientheroku.s3-website.ca-central-1.amazonaws.com",
   /\.amazonaws\.com$/,
+  /.amazonaws\.com$/,
   "http://52.60.65.223",
   "http://52.60.65.223/",
 ]; // allowed domains
