1e1d899802f448fdd522ec36890bbfdabb4dfe00
pwela <139175113+pwela@users.noreply.github.com>
2024-02-28 13:16:28 -0700

    edit cors

--- index.js
+++ index.js
@@ -118,10 +118,7 @@ let alloweddOrigins = [
   "http://myflixbucket-02182024.s3-website.ca-central-1.amazonaws.com/",
   "http://load-balancer-task-2-2-1273969783.ca-central-1.elb.amazonaws.com",
   "http://myflixclientheroku.s3-website.ca-central-1.amazonaws.com",
-  /amazonaws\.com$/,
-  /.amazonaws\.com$/,
-  /\.amazonaws\.com$/,
-  /\amazonaws\.com$/,
+
   "http://52.60.65.223",
   "http://52.60.65.223/",
 ]; // allowed domains
